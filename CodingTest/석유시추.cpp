36a19c36ca3d157852c0ad7cebc5f376fac8001e
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2024-11-01 23:14:46 +0900

    석유시추 효율성 통과 조건 주석추가

--- CodingTest/석유시추.cpp
+++ CodingTest/석유시추.cpp
@@ -1,11 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/250136
 
+// 효율성 통과를 위해선 석유 덩어리에 번호를 매겨보자
+
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
 vector<pair<int, int>> Direction = { {1,0},{0,1}, {-1,0}, {0,-1}};
+vector<vector<int>> DP;
 
 void DFS(int x, int y, int& Count, vector<vector<int>>& land)
 {
@@ -15,15 +19,19 @@ void DFS(int x, int y, int& Count, vector<vector<int>>& land)
     {
         int NewX = x + d.first;
         int NewY = y + d.second;
-        if (NewX < 0 || NewY < 0 || NewX >= land[0].size() || NewY >= land.size() || !land[NewY][NewX]) continue;
+        if (NewX < 0 || NewY < 0 || NewX >= land[0].size() || NewY >= land.size() || land[NewY][NewX] != 1) continue;
 
         DFS(NewX, NewY, Count, land);
     }
+
+    DP[y][x] = max(Count, DP[y][x]);
 }
 
 int solution(vector<vector<int>> land)
 {
     int answer = 0;
+
+    DP.assign(land.size(), vector<int>(land[0].size(), 0));
 
     for (int j = 0; j < land[0].size(); j++)
     {
@@ -36,8 +44,6 @@ int solution(vector<vector<int>> land)
         }
         answer = max(Count, answer);
     }
-
-
     return answer;
 }
 

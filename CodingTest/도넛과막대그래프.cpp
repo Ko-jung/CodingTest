2de5238fb6d3116c0fc151a98125eb15117762d0
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2024-11-01 22:18:58 +0900

    프로그래머스 도넛과막대그래프 문제 추가 (답지 확인)

---
+++ CodingTest/도넛과막대그래프.cpp
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/258711
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> edges) {
+    vector<int> answer;
+
+
+
+    return answer;
+}
+
+int main()
+{
+    solution({{2, 3}, {4, 3}, {1, 1}, {2, 1}} );
+    solution({{4, 11}, {1, 12}, {8, 3}, {12, 7},
+        {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11},
+        {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9},
+        {3, 8}} );
+}

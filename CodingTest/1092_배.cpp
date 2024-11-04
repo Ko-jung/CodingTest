288aaa58aacb303902f7098acd8860d5568304a4
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2024-11-05 02:09:27 +0900

    1092_배 해결 완료

--- CodingTest/1092_배.cpp
+++ CodingTest/1092_배.cpp
@@ -10,6 +10,63 @@
 
 // 밑에 생각한 로직
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> Crane(N);
+	for (auto& C : Crane)
+	{
+		cin >> C;
+	}
+	sort(Crane.begin(), Crane.end(), std::greater());
+
+	int M;
+	cin >> M;
+
+	vector<int> Box(M);
+	for (auto& B : Box)
+	{
+		cin >> B;
+	}
+	sort(Box.begin(), Box.end(), std::greater());
+
+	if (Box[0] > Crane[0])
+	{
+		cout << "-1";
+		return 0;
+	}
+
+	int ProcessedBox{ 0 };
+	int LoopCount{ 0 };
+	while (ProcessedBox < M)
+	{
+		int i = 0;
+		for (auto& C : Crane)
+		{
+			for (; i < M; ++i)
+			{
+				if (Box[i] == -1) continue;
+				if (C >= Box[i])
+				{
+					++ProcessedBox;
+					Box[i] = -1;
+					break;
+				}
+			}
+		}
+		LoopCount++;
+	}
+
+	cout << LoopCount;
+}
 
 
 

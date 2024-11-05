b07a10b8264aa86459c0d813b48a1dc1b7e585f6
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2024-11-06 04:47:51 +0900

    25307_시루의백화점구경 해결 실패 (메모리 초과, 답지 확인)

--- CodingTest/25307_시루의백화점구경.cpp
+++ CodingTest/25307_시루의백화점구경.cpp
@@ -13,5 +13,104 @@
 //
 //시루는 다리가 너무 아프기 때문에 체력 소모를 최소화하면서 의자까지 찾아가려고 한다.시루가 소모하는 체력의 최솟값을 구해보자.
 
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Position
+{
+	int X;
+	int Y;
+	int Cost;
+};
+
+bool IsNear(int x, int y, int TargetX, int TargetY, int dist)
+{
+	return (abs(x - TargetX) + abs(y - TargetY)) <= dist;
+}
+
+int main()
+{
+	vector<pair<int, int>> Direction = {
+		{1,0},{0,1},{-1,0},{0,-1}
+	};
+
+	int M, N, K;
+	cin >> N >> M >> K;
+
+	//vector<pair<int, int>> Mannequin;
+	queue<Position> MannequinBFS;
+	vector<vector<int>> Board(N, vector<int>(M, 0));
+	vector<vector<bool>> Banned(N, vector<bool>(M, false));
+	vector<vector<bool>> Visited(N, vector<bool>(M, false));
+	pair<int, int> StartPos;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> Board[i][j];
+
+			if (Board[i][j] == 4)
+				StartPos = { j,i };
+			if (Board[i][j] == 3)
+			{
+				MannequinBFS.push({ j,i,K });
+				//Mannequin.emplace_back(pair<int, int>{ j,i });
+			}
+		}
+	}
+
+	// 마네킹의 위치를 바탕으로 못가는 위치 정하기
+	while (!MannequinBFS.empty())
+	{
+		Position Pos = MannequinBFS.front();
+		MannequinBFS.pop();
+
+		Banned[Pos.Y][Pos.X] = true;
+		if (Pos.Cost <= 0) continue;
+
+		for (const auto& d : Direction)
+		{
+			int NewX = Pos.X + d.first;
+			int NewY = Pos.Y + d.second;
+
+			if (NewX < 0 || NewY < 0 || NewX >= M || NewY >= N || Banned[NewY][NewX]) continue;
+
+			MannequinBFS.push({ NewX, NewY, Pos.Cost - 1 });
+		}
+	}
+
+	queue<Position> BFS;
+	BFS.push({StartPos.first, StartPos.second, 0});	
+	while (!BFS.empty())
+	{
+		Position Pos = BFS.front();
+		BFS.pop();
+
+		Visited[Pos.Y][Pos.X] = true;
+		if (Board[Pos.Y][Pos.X] == 2)
+		{
+			cout << Pos.Cost;
+			return 0;
+		}
+
+		for (const auto& d : Direction)
+		{
+			int NewX = Pos.X + d.first;
+			int NewY = Pos.Y + d.second;
+
+			if (NewX < 0 || NewY < 0 || NewX >= M || NewY >= N || Visited[NewY][NewX]
+				|| Board[NewY][NewX] == 1) continue;
+
+			if (Banned[NewY][NewX]) continue;
+
+			BFS.push({ NewX, NewY, Pos.Cost + 1 });
+		}
+	}
+
+	cout << "-1";
+}
 
 // 1. 

05459b191981631375d02d843988ad2c500c8715
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2025-04-03 00:28:04 +0900

    1030_프렉탈평면 풀이 실패
    답지 확인 후 참고해서 작성하여 메모리 초과는 안 뜨지만 틀린 결과가 도출

--- CodingTest/1030_프렉탈평면.cpp
+++ CodingTest/1030_프렉탈평면.cpp
@@ -11,63 +11,62 @@
 #include <vector>
 #include <cmath>
 
-void Recall(std::vector<std::vector<bool>>& Board, const int& x, const int& y, const int& S, const int& N, const int& K,  const int& Depth, const bool& Answer)
+int S, N, K;
+
+bool Recall(const int& x, const int& y, const int& Depth)//, const bool& Answer)
 {
-	if (Depth == S)
+	if (Depth == 0) return false;
+	int Interval = (N - K) / 2;
+	if ((x / N) * N + Interval <= x && x < (x / N) * N + (N - Interval)
+		&& (y / N) * N + Interval <= y && y < (y / N) * N + (N - Interval))
 	{
-		Board[y][x] = Answer;
-		return;
+		return true;
 	}
-	else
-	{
-		int Len = std::pow(N, Depth);
-		int Interval = (N - K) / 2;
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < N; j++)
-			{
-				if (Answer || (Interval <= j && j < (N - Interval)
-					&& Interval <= i && i < (N - Interval)))
-				{
-					Recall(Board, x * N + j, y * N + i, S, N, K, Depth + 1, true);
-				}
-				else
-				{
-					Recall(Board, x * N + j, y * N + i, S, N, K, Depth + 1, false);
-				}
-			}
-		}
-	}
+	return Recall(x / N, y / N, Depth - 1);
+
+	//if (Depth == S)
+	//{
+	//	Board[y][x] = Answer;
+	//	return;
+	//}
+	//else
+	//{
+	//	int Len = std::pow(N, Depth);
+	//	int Interval = (N - K) / 2;
+	//	for (int i = 0; i < N; i++)
+	//	{
+	//		for (int j = 0; j < N; j++)
+	//		{
+	//			if (Answer || (Interval <= j && j < (N - Interval)
+	//				&& Interval <= i && i < (N - Interval)))
+	//			{
+	//				Recall(Board, x * N + j, y * N + i, S, N, K, Depth + 1, true);
+	//			}
+	//			else
+	//			{
+	//				Recall(Board, x * N + j, y * N + i, S, N, K, Depth + 1, false);
+	//			}
+	//		}
+	//	}
+	//}
 }
 
 int main()
 {
-	int S, N, K, R1, R2, C1, C2;
+	int R1, R2, C1, C2;
 	std::cin >> S >> N >> K >> R1 >> R2 >> C1 >> C2;
 
-	int Line = std::pow(N, S);
-	std::vector<std::vector<bool>> Board(Line, std::vector<bool>(Line, false));
-
-	Recall(Board, 0, 0, S, N, K, 0, false);
-	//for (int i = 1; i <= S; i++)
-	//{
-	//	int Len = std::pow(N, i);
-	//	std::vector<std::vector<bool>> b(Len, std::vector<bool>(Len, false));
-	//	for (int y = 0; y < Len; y++)
-	//	{
-	//		for (int x = 0; x < Len; x++)
-	//		{
-	//			int Interval = (N - K) / 2;
-	//			if(Interval <= x < Len)
-	//		}
-	//	}
-	//}
+	if (S == 0)
+	{
+		std::cout << "0";
+		return 0;
+	}
 
 	for (int j = R1; j <= R2; j++)
 	{
 		for (int i = C1; i <= C2; i++)
 		{
-			std::cout << Board[i][j] << " ";
+			std::cout << Recall(i, j, S) << " ";
 		}
 		std::cout << std::endl;
 	}

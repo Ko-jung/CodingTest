d5cfe4e5549f9810623ee1d976ffbc42c2f98b60
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2025-09-18 21:00:46 +0900

    bool로 수정 성공

--- CodingTest/14217_그래프탐색.cpp
+++ CodingTest/14217_그래프탐색.cpp
@@ -2,21 +2,22 @@
 #include <vector>
 #include <queue>
 #include <limits>
+#include <map>
 
 using namespace std;
 
 constexpr int MAX = numeric_limits<int>::max();
 
-void Print(const vector<vector<int>>& Board)
+void Print(const vector<vector<bool>>& Board)
 {
-
+	cout << "======================" << endl;
 	for (auto board : Board)
 	{
 		for (auto b : board)
-			cout << (b == MAX ? -1 : b) << ", ";
+			cout << (b ? 1 : 0) << ", ";
 		cout << endl;
 	}
-	cout << endl;
+	cout << "======================" << endl;
 }
 
 int main()
@@ -42,63 +43,63 @@ int main()
 		Plans.emplace_back(a, k, j);
 	}
 
-	vector<vector<int>> Board(N + 1, vector<int>(N + 1, MAX));;
+	vector<vector<bool>> Board(N + 1, vector<bool>(N + 1, false));;
 	for (int i = 0; i < Board.size(); i++)
 	{
-		Board[i][i] = 0;
+		Board[i][i] = true;
 	}
 	for (const auto& road : Roads)
 	{
-		Board[road.first][road.second] = 1;
-		Board[road.second][road.first] = 1;
+		Board[road.first][road.second] = true;
+		Board[road.second][road.first] = true;
 	}
 
-	// Plan으로 도로가 수정되는 것을 반영
-	vector<vector<int>> FixBoard(Board.begin(), Board.end());
 	for (const auto& plan : Plans)
 	{
 		auto [a, i, j] = plan;
 		if (a == 1)
 		{
-			FixBoard[i][j] = 1;
-			FixBoard[j][i] = 1;
+			Board[i][j] = true;
+			Board[j][i] = true;
 		}
 		else
 		{
-			FixBoard[i][j] = MAX;
-			FixBoard[j][i] = MAX;
+			Board[i][j] = false;
+			Board[j][i] = false;
 		}
+		//Print(Board);
 
-		// 수도로부터 얼마나 거리가 반영
-		vector<vector<int>> Dijkstar(FixBoard.begin(), FixBoard.end());
-
-		//Dijkstar
+		//Board
 		priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> BFS;
 		BFS.emplace(0, 1, 1);
 		vector<bool> Visited(N + 1, false);
+		vector<int> Ans(N + 1, 0);
+		Ans[1] = 0;
+		Visited[1] = true;
 		while (!BFS.empty())
 		{
 			auto [Weight, Prev, Now] = BFS.top();
 			BFS.pop();
 
 			//cout << Weight << ", " << Prev << ", " << Now << endl;
-
-			Dijkstar[1][Now] = min(Dijkstar[1][Now], Weight);
-			Visited[Now] = true;
+			Ans[Now] = Weight;
 
 			for (int i = 1; i <= N; i++)
 			{
-				if (Now == i ||Dijkstar[Now][i] == MAX || Visited[i]) continue;
-				if (Weight + Dijkstar[Now][i] > Dijkstar[1][i]) continue;
-				BFS.emplace(Weight + Dijkstar[Now][i], Now, i);
+				if (Now == i || !Board[Now][i] || Visited[i]) continue;
+				Visited[i] = true;
+				BFS.emplace(Weight + 1, Now, i);
 			}
-		}
 
-		cout	<< (Dijkstar[1][1] == MAX ? -1 : Dijkstar[1][1]) << " "
-				<< (Dijkstar[1][2] == MAX ? -1 : Dijkstar[1][2]) << " "
-				<< (Dijkstar[1][3] == MAX ? -1 : Dijkstar[1][3]) << " "
-				<< (Dijkstar[1][4] == MAX ? -1 : Dijkstar[1][4]) << " "
-				<< (Dijkstar[1][5] == MAX ? -1 : Dijkstar[1][5]) << endl;
+		}
+		for (int i = 1; i <= N; i++)
+		{
+			if(Visited[i])
+				cout << Ans[i] << " ";
+			else
+				cout << -1 << " ";
+		}
+		cout << endl;
 		//cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
 	}
 }
